bf546f09b92420eb44686f50c72b2aec663184ce
Knublaks <rihardsos22@gmail.com>
2024-06-25 17:17:49 +0300

    Add files via upload

---
+++ jautajumi.js
@@ -0,0 +1,14 @@
+// [ jautajums, pareizā atbilde, atbilde A, atbilde B, atbilde C]
+
+var jautajumi=[
+['1. Kāds ir pareizais HTML elements, lai izveidotu saiti?','2','a) <link>','b) <a>','c) <href>','d) <s>'],
+['2. Kuru HTML elementu izmanto, lai parādītu attēlu?','1','a) <img>','b) <image>','c) <pic>','d) <src>'],
+['3. Kurš HTML elements tiek izmantots, lai izveidotu sarakstu ar punktiem?','3','a) <ol>','b) <li>','c) <ul>','d) <dl>'],
+['4. Kurš CSS īpašības nosaukums nosaka teksta krāsu?','4','a) text-color','b) font-color','c) background-color','d) color'],
+['5. Kura CSS īpašība maina elementa fona krāsu?','1','a) background-color','b) color','c) border-color','d) bg-color'],
+['6. Kura CSS īpašība nosaka elementa ārējo attālumu?','3','a) padding','b) border','c) margin','d) spacing'],
+['7. Kā var deklarēt mainīgo JavaScript?','4','a) var','b) let','c) const','d) Visi iepriekš minētie'],
+['8. Kas tiek izmantots, lai parādītu paziņojumu logā JavaScript?','1','a) alert()','b) msg()','c) prompt()','d) console.log()'],
+['9. Kāds ir pareizais veids, kā norādīt komentāru JavaScript?','3','a) <!-- Komentārs -->','b) # Komentārs','c) // Komentārs','d) ** Komentārs **'],
+['10. Kādu funkciju izmanto, lai pārveidotu tekstu par lielajiem burtiem JavaScript?','2','a) toBigLetters()','b) toUpperCase()','c) makeUpperCase()','d) capitalize()']
+];
